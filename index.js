4cb603ad5a3a94ec923c23bfcc198088575badd4
Mike <103404760+moistmike101@users.noreply.github.com>
2023-07-10 23:25:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ function guestScore3() {
 
 function resetBtn() {
     let endResult = 0
-    document.getElementById("guest-Number").textContext = 0
-    document.getElementById("home-Number").textContext = 0
+    document.reset(gusNumber)
+    document.reset(gusNumber)
+    
 }
